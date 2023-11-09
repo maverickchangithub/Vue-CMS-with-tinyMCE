bbf1ea0e439626d97434b067cad2d12f1b44808b
Maverick Chan <maverick_4@msn.com>
2023-11-09 17:46:47 +0800

    innerHTML? prism still broken

--- src/views/editor/MceEditor.vue
+++ src/views/editor/MceEditor.vue
@@ -9,14 +9,7 @@
     </div>
 </template>
 <script setup>
-    import Prism from 'prismjs'
-    import 'prismjs/components/prism-javascript' // Language
-    import 'prismjs/components/prism-php'
-    import 'prismjs/components/prism-markup'
-    import 'prismjs/components/prism-swift'
-    import 'prismjs/components/prism-sql'
-    // import 'prismjs/themes/prism.css'
-    import 'prismjs/themes/prism-twilight.css' // Theme
+    
     import { nextTick, ref, onMounted } from 'vue';
     import * as editor from '../../editor';
     const body = ref('')
@@ -24,7 +17,7 @@
         editor.render();        
     });
     onMounted(() => {
-        Prism.highlightAll()
+        
     })
 </script>
 <style scoped>
